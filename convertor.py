499d4dc3cff2addce9a979a20dec1739d8647a2a
DevMusengah <123998676+Musengah@users.noreply.github.com>
2024-01-15 18:51:14 +0200

    Add files via upload
    Added a python code used to create a database and single table using the csv file provided.

---
+++ convertor.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+postgresql_username = ''
+postgresql_password = ''
+postgresql_host = ''
+postgresql_database = 'spotify_data'
+
+database_url = f'postgresql+psycopg2://{postgresql_username}:{postgresql_password}@{postgresql_host}/{postgresql_database}'
+engine = create_engine(database_url)
+
+csv_file_path = 'spotify-data.csv'
+
+try:
+    df = pd.read_csv(csv_file_path)
+    table_name = 'music'
+    df.to_sql(table_name, engine, index=False, if_exists='replace')
+except Exception as e:
+    print(f"Error: {e}")
+
+engine.dispose()
